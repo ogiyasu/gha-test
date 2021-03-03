2ef1c077607b14947cc661da7e00b371944fb6c1
YasuhiroOgino <yasuhiro.ogino@konel.jp>
2021-03-03 09:28:57 +0900

    initial commit

---
+++ pages/index.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="container">
+    <div>
+      <Logo />
+      <p class="title">
+        Let's try Github Actions!
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.title {
+  font-family:
+    'Quicksand',
+    'Source Sans Pro',
+    -apple-system,
+    BlinkMacSystemFont,
+    'Segoe UI',
+    Roboto,
+    'Helvetica Neue',
+    Arial,
+    sans-serif;
+  display: block;
+  font-weight: 300;
+  font-size: 30px;
+  color: #000000;
+  letter-spacing: 1px;
+}
+
+.subtitle {
+  font-weight: 300;
+  font-size: 42px;
+  color: #526488;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
+
+.links {
+  padding-top: 15px;
+}
+</style>
